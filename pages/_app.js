54280e5cf937dabca8d8986bfdc09d11c2001723
auto-mausx <auto-mausx@gmail.com>
2022-01-12 18:09:43 -0700

    baseline for page routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <div>
+      <nav>
+        <p>PulseMarket</p>
+        <div>
+          <Link href="/">
+            <a>
+              Browse NFTs
+            </a>
+          </Link>
+          <Link href="/collection">
+            <a>
+              Your Collection
+            </a>
+          </Link>
+          <Link href="/wallet-connect">
+            <a>
+              Connect Wallet
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+    ) 
 }
 
 export default MyApp
