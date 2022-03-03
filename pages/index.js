3f13e00fcbaa28346bd6892f229abad3e3ff03f7
auto-mausx <auto-mausx@gmail.com>
2022-03-03 15:53:22 -0700

    latest update

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,19 @@ import Login from './Login';
 
 export default function Home() {
   const [isLoggedin, setLoginStatus] = useState(true);
+  const providerOptions = {
+    // https://github.com/Web3Modal/web3modal
+  }
+
+  const web3Modal = new Web3Modal({
+    network: "mainnet",
+    cacheProvider: true,
+    providerOptions
+  })
+
+  const instance = await web3Modal.connect();
+  const provider = new ethers.providers.Web3Provider(instance);
+  const signer = provider.getSigner();
 
   useEffect(() => {
     setLoginStatus(false);
